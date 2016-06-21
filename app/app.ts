d26e5135bd0cde7b3e9dec1a7785b46e4d63b537
TigeR <a.linck@onlinehome.de>
2016-06-21 19:51:01 +0200

    Added ngStorage to access LocalStorage

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 
 module WorldBuilder {
-    angular.module("WorldBuilder", ["ngRoute"])
+    angular.module("WorldBuilder", ["ngRoute", "ngStorage"])
         .config(["$locationProvider", "$routeProvider", ($locationProvider: ng.ILocationProvider, $routeProvider: ng.route.IRouteProvider) => {
             $locationProvider.hashPrefix("!");
             $routeProvider.otherwise({ redirectTo: "/view1" });
