48703c0dd00567a6a199133466063675d8116350
TigeR <a.linck@onlinehome.de>
2016-06-22 16:55:53 +0200

    Added editable project title

--- app/app.ts
+++ app/app.ts
@@ -39,5 +39,27 @@ module WorldBuilder {
         }])
         .filter("escape", ["$window", ($window) => {
             return $window.encodeURIComponent;
+        }])
+        .directive('wbClickOff', ["$parse", "$document", function($parse, $document) {
+            // http://anand.codes/projects/click-off/
+            return {
+                compile: function($element, attr) {
+                    // Parse the expression to be executed
+                    // whenever someone clicks _off_ this element.
+                    var fn = $parse(attr["wbClickOff"]);
+                    return function(scope, element, attr) {
+                        // add a click handler to the element that
+                        // stops the event propagation.
+                        element.bind("click", function(event) {
+                            event.stopPropagation();
+                        });
+                        angular.element($document[0].body).bind("click", function(event) {
+                            scope.$apply(function() {
+                                fn(scope, {$event:event});
+                            });
+                        });
+                    };
+                }
+            };
         }]);
 }
