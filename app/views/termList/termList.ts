b0a6015b6514b9231c8ae1dcdfca028108d0d4e4
TigeR <a.linck@onlinehome.de>
2016-06-27 18:50:40 +0200

    Moved shared code in termList.ts to Util namespace

--- app/views/termList/termList.ts
+++ app/views/termList/termList.ts
@@ -12,23 +12,17 @@ module WorldBuilder {
      * Controller for the term overview.
      */
     export class TermListController {
-        static $inject = ["$scope", "$localStorage", "$routeParams"];
+        static $inject = ["$scope", "$localStorage", "$routeParams", "$location"];
 
-        constructor(private $scope: TermListScope, $localStorage: ng.storage.IStorageService, $routeParams: ProjectRouteParams) {
-            this.$scope.$storage = <ProjectStorage>$localStorage.$default({
-                projects: []
-            });
-
+        constructor(private $scope: TermListScope, $localStorage: ng.storage.IStorageService, $routeParams: ProjectRouteParams, $location: ng.ILocationService) {
+            this.$scope.$storage = <ProjectStorage>$localStorage.$default({ projects: [] });
             this.$scope.project = this.$scope.$storage.projects.filter((p) => p.guid === $routeParams.project)[0];
+            if (!this.$scope.project) {
+                $location.path("/projects");
+            }
         }
     }
 
-    angular.module("WorldBuilder")
-        .config(["$routeProvider", ($routeProvider: ng.route.IRouteProvider) => {
-            $routeProvider.when("/projects/:project/terms", {
-                templateUrl: "views/termList/termList.html",
-                controller: "TermListController"
-            });
-        }])
-        .controller("TermListController", TermListController);
+    Util.registerAngularController("TermListController", TermListController,
+        "/projects/:project/terms", "views/termList/termList.html");
 }
