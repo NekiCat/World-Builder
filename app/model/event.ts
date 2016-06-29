473fd57fd6a424c44ac94b4455209420a014adea
TigeR <a.linck@onlinehome.de>
2016-06-29 19:51:52 +0200

    Added list capability to events

--- app/model/event.ts
+++ app/model/event.ts
@@ -1,7 +1,8 @@
 module WorldBuilder {
-    export class Event implements Identifiable, Nameable {
+    export class Event implements Identifiable, Nameable, Deletable {
         guid: string = Util.createGUID();
         name: string = "New Event";
+        deleted: boolean = false;
         icon: Icon;
     }
 }
--- app/views/eventList/eventList.ts
+++ app/views/eventList/eventList.ts
@@ -3,7 +3,7 @@ module WorldBuilder {
     /**
      * Interface for the scope of an EventListController.
      */
-    export interface EventListScope extends ng.IScope {
+    export interface EventListScope extends CommonListScope<Event> {
         $storage: ProjectStorage;
         project: Project;
     }
@@ -15,11 +15,19 @@ module WorldBuilder {
         static $inject = ["$scope", "$localStorage", "$routeParams", "$location"];
 
         constructor(private $scope: EventListScope, $localStorage: ng.storage.IStorageService, $routeParams: ProjectRouteParams, $location: ng.ILocationService) {
-            this.$scope.$storage = <ProjectStorage>$localStorage.$default({ projects: [] });
-            this.$scope.project = this.$scope.$storage.projects.filter((p) => p.guid === $routeParams.project)[0];
-            if (!this.$scope.project) {
+            $scope.$storage = <ProjectStorage>$localStorage.$default({ projects: [] });
+            $scope.project = this.$scope.$storage.projects.filter((p) => p.guid === $routeParams.project)[0];
+            if (!$scope.project) {
                 $location.path("/projects");
             }
+
+            $scope.createItem = () => Util.createNameable($scope.project.events, Event);
+            $scope.removeItem = (e) => Util.removeStart($scope.project.events, e);
+            $scope.removeUndo = (e) => Util.removeUndo($scope.project.events, e);
+            $scope.$on('$locationChangeSuccess', () => {
+                Util.removeEnd($scope.project.events);
+                $localStorage.$apply();
+            });
         }
     }
 
